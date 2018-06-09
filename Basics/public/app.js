b58cebd817b2f4437dfd9c91b0a756556a3e86ea
vivekgondaliya <vg13@zips.uakron.edu>
2018-06-09 16:13:20 -0700

    Real time data change updates the DOC

--- Basics/public/app.js
+++ Basics/public/app.js
@@ -4,22 +4,23 @@ document.addEventListener("DOMContentLoaded", event => {
   const app = firebase.app();
   console.log(app);
 
-  function googleLogin(){
-    //provider that we want to use, in this case - Google
-    const provider = new firebase.auth.GoogleAuthProvider();
-
-    //Enable login with Google
-    firebase.auth().signInWithPopup(provider)
-      .then(result => {
-        const user = result.user;
-        document.write('Hello ' + user.displayName);
-        console.log(user);
-      })
-      .catch(console.log);
-
-  }
-
-  document.getElementById ("login").addEventListener ("click", googleLogin, false);
+  // USER AUTHENTICATION
+  // function googleLogin(){
+  //   //provider that we want to use, in this case - Google
+  //   const provider = new firebase.auth.GoogleAuthProvider();
+  //
+  //   //Enable login with Google
+  //   firebase.auth().signInWithPopup(provider)
+  //     .then(result => {
+  //       const user = result.user;
+  //       document.write('Hello ' + user.displayName);
+  //       console.log(user);
+  //     })
+  //     .catch(console.log);
+  //
+  // }
+  //
+  // document.getElementById ("login").addEventListener ("click", googleLogin, false);
 
   //Firestore DB
   const db = firebase.firestore();
@@ -27,10 +28,17 @@ document.addEventListener("DOMContentLoaded", event => {
   db.settings(settings);
   const myPost = db.collection('posts').doc('firepost');
 
-  myPost.get()
-    .then(doc => {
-      const data = doc.data();
-      document.write(data.title + '</br>');
-      document.write(data.views);
-    });
+  // myPost.get()
+  //   .then(doc => {
+  //     const data = doc.data();
+  //     document.write(data.title + '</br>');
+  //     document.write(data.views);
+  //   });
+
+  //Listening for REAL-TIME Data Change
+  myPost.onSnapshot(doc => {
+    const data = doc.data();
+    document.write(data.title + '</br>');
+    document.write(data.views + '</br>');
+  });
 });
